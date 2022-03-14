5b470688c7ee31bc71ce61d451ffad4a200ac07f
Carlos Moreno Quintanar <carlos.sigma66@gmail.com>
2022-03-13 23:04:37 -0600

    Actualizcion de Descripcion del proyecto en index.py

--- index.py
+++ index.py
@@ -1,3 +1,7 @@
+# La ruta del proyecto es: C:\Users\carlo\OneDrive\Cursos Practicos\Python\Flask Fast Web Mar 2022
+# Tiene Virutal Environment llamado website
+# Se utiliza Git y Github: https://github.com/carlosreynosa15514/website_flask.github.io
+
 # Importando libreria flask
 from flask import Flask
 # Para poder utilizar files html en los paths creados
